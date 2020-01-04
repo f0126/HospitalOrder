5327193e0270c9a12d9d17b01223b3b1376158e1
xin <24537qq.com>
2020-01-04 10:38:15 +0800

    增加预约单页面

--- src/main/java/com/woniu/controller/BookingFormController.java
+++ src/main/java/com/woniu/controller/BookingFormController.java
@@ -1,6 +1,28 @@
 package com.woniu.controller;
 
+import java.util.List;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.woniu.domain.Bookingform;
+import com.woniu.service.BookingFormService;
+
+@Controller
+@RequestMapping("bookingform")
 public class BookingFormController {
-   
+	@Autowired 
+	private BookingFormService bookingserver;
+    @RequiresPermissions("bookingform:find")
+    @RequestMapping("find")
+    public String find(Model model) {
+		List<Bookingform> list = bookingserver.find();
+		model.addAttribute("list", list);
+		return "/admin/bookingform/findUI";
+    }
+    
 	
 }
--- src/main/webapp/commons/taglib.jsp
+++ src/main/webapp/commons/taglib.jsp
@@ -4,3 +4,4 @@
 <%@ taglib uri="http://shiro.apache.org/tags" prefix="shiro" %>
 <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
 <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
